aec937a0411632dd4f9b3fab0469ce9c4c24e62a
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-08 16:23:32 -0700

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main-Entry source
  * 
- * Return:Always 0 (Success)
+ * Return:Always0(Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
