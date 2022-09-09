4a32248a31f229184d309137f2ec329d17e1bfd6
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-08 17:44:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
+
 /**
- * main-Prints "and that piece of art is useful" -Dora Korpar, 2015-10-19",
+ * main-Prints a text
  *
- * Return:Always 1
+ * Return:Always 1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
+	write(2,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
