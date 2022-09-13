4c7d1102efd0b9b6319e32237625f0991b636975
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-13 15:22:26 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,17 @@
 
 /**
  * main - print_alphabet
+ *
  * Return: 0
  */
 
 void print_alphabet(void)
 {
-	char c = 'a';
+	char c;
 
-	while (c <= 'z')
-	{ 
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
-		c++;
-
 	}
-	_putchar('\n');
+_putchar('\n');
 }
