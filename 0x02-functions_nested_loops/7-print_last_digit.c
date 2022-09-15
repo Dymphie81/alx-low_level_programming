7a34e9a8e1360e6b5b136c4db71bab3f04063f1b
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-14 21:23:47 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit of a number
+ * @n: input letter
+ * Return: Alwys success
+ */
+int print_last_digit(int n)
+{
+
+int p;
+p = (n % 10);
+}
+
+_putchar(p + '0');
+
+return (p);
+}
