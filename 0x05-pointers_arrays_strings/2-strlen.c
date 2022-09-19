50c39ad14fa01c4bca703468cbf662676591e655
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-19 14:03:14 -0700

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: string to count
+ *
+ * Return: string length
+ */
+int_strlen(char *s)
+{
+	int < = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
