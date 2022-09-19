0717c8c28516cd08de68f2e10514fe985b56d924
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-19 13:23:11 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include"main.h"
 
 /**
- * reset_to_98 - updates the value it points to 98
+ * reset_to_98 - functions that takes a pointer to an int as parameter and
+ * updates the value it points to 98
  *
  * @n: pointer to n
  *
- * Return: void
  */
 
 void reset_to_98(int *n)
