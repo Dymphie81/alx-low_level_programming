b75c0f7e94560d0b1e28af86f2ae6f56fd83e14a
Ifunanya Egbuniwe <egbuniwemela81@gmail.com>
2022-09-19 12:51:07 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 
 /**
  * reset_to_98 - updates the value it points to 98
+ *
  * @n: pointer to n
- * 
- * int_putchar(char c);
  *
  * Return: void
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
